64819ec13e3be7ffc29cfdc8e957e2dc786c0c15
tranhongtri <108580990+tranhongtri@users.noreply.github.com>
2023-05-03 11:37:07 +0700

    revert lazy load

--- src/App.js
+++ src/App.js
@@ -1,59 +1,63 @@
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect, lazy, Suspense } from "react";
+import { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import ContextProvider from "./Context/ContextProvider";
+import About from "./Pages/About/About";
+import AboutTwo from "./Pages/About/AboutTwo";
+// import Blog from "./Pages/Blog/Blog";
+// import BlogTwo from "./Pages/Blog/BlogTwo";
+import Contact from "./Pages/Contact/Contact";
+// import ContactTwo from "./Pages/Contact/ContactTwo";
+import Home from "./Pages/Home/Home";
+import HomePage from "./Pages/Home/HomePage";
+import HomeTwo from "./Pages/Home/HomeTwo";
+import Portfilo from "./Pages/Portfilo/Portfilo";
+import PortfiloTwo from "./Pages/Portfilo/PortfiloTwo";
+import Personal from "./Pages/Personal/Personal";
+import PersonalTwo from "./Pages/Personal/PersonalTwo";
+// import CertificateTwo from "./Pages/Certificate/CertificateTwo";
+import Resume from "./Pages/Resume/Resume";
+import ResumeTwo from "./Pages/Resume/ResumeTwo";
 import NotFound from "./Share/NotFound";
 import Preview from "./Share/Preview";
-
-const About = lazy(() => import("./Pages/About/About"));
-const AboutTwo = lazy(() => import("./Pages/About/AboutTwo"));
-const Contact = lazy(() => import("./Pages/Contact/Contact"));
-const Home = lazy(() => import("./Pages/Home/Home"));
-const HomePage = lazy(() => import("./Pages/Home/HomePage"));
-const HomeTwo = lazy(() => import("./Pages/Home/HomeTwo"));
-const Portfilo = lazy(() => import("./Pages/Portfilo/Portfilo"));
-const PortfiloTwo = lazy(() => import("./Pages/Portfilo/PortfiloTwo"));
-const Personal = lazy(() => import("./Pages/Personal/Personal"));
-const PersonalTwo = lazy(() => import("./Pages/Personal/PersonalTwo"));
-const Resume = lazy(() => import("./Pages/Resume/Resume"));
-const ResumeTwo = lazy(() => import("./Pages/Resume/ResumeTwo"));
 
 function App() {
   useEffect(() => {
     AOS.init({ duration: 1200 });
     AOS.refresh();
   }, []);
-
   return (
     <>
       <ContextProvider>
         <BrowserRouter>
-          <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-              <Route path="/preview" element={<Preview />} />
-              <Route path="/home" element={<Home />}>
-                <Route index element={<HomePage />} />
-                <Route path="homePage" element={<HomePage />} />
-                <Route path="resume" element={<Resume />} />
-                <Route path="about" element={<About />} />
-                <Route path="contact" element={<Contact />} />
-                <Route path="works" element={<Portfilo />} />
-                <Route path="personal" element={<Personal />} />
-              </Route>
-              <Route path="/" element={<HomeTwo />}>
-                <Route index element={<AboutTwo />} />
-                <Route path="about" element={<AboutTwo />} />
-                <Route path="resume" element={<ResumeTwo />} />
-                <Route path="work" element={<PortfiloTwo />} />
-                <Route path="personal" element={<PersonalTwo />} />
-              </Route>
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </Suspense>
+          <Routes>
+            <Route path="/preview" element={<Preview />} />
+            <Route path="/home" element={<Home />}>
+              <Route index element={<HomePage />} />
+              <Route path="homePage" element={<HomePage />} />
+              <Route path="resume" element={<Resume />} />
+              <Route path="about" element={<About />} />
+              <Route path="contact" element={<Contact />} />
+              {/* <Route path="blogs" element={<Blog />} /> */}
+              <Route path="works" element={<Portfilo />} />
+              <Route path="personal" element={<Personal />} />
+            </Route>
+            <Route path="/" element={<HomeTwo />}>
+              <Route index element={<AboutTwo />} />
+              <Route path="about" element={<AboutTwo />} />
+              <Route path="resume" element={<ResumeTwo />} />
+              {/* <Route path="certificate" element={<CertificateTwo />} /> */}
+              {/* <Route path="blogs" element={<BlogTwo />} /> */}
+              <Route path="work" element={<PortfiloTwo />} />
+              <Route path="personal" element={<PersonalTwo />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </BrowserRouter>
       </ContextProvider>
+
       <ToastContainer
         position="top-right"
         autoClose={5000}
